d0771c5b08c283907b1453e108273357799e2824
Sanket Panda <pandasa123@gmail.com>
2020-03-10 13:03:29 -0400

    Added Calendar and To-Do pages to view directory

---
+++ src/views/Todo.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="px-2 py-8 mx-auto bg-gray-100 max-w-7xl sm:px-6 lg:px-8">
+		<h1>To-Do Page</h1>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Todo',
+};
+</script>
