35e4e2d34edc0a98970b624ca4c77a5c6ade7608
Sanket Panda <pandasa123@gmail.com>
2020-03-10 19:08:05 -0400

    Started OAuth work!

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import App from './App.vue';
 import './assets/tailwind.css';
 import router from './router';
 import vco from 'v-click-outside';
+import GAuth from 'vue-google-oauth2';
 
 Vue.use(vco);
+
+const gAuthOption = {
+	clientId: process.env.VUE_APP_GAPI,
+	scope: 'https://www.googleapis.com/auth/calendar',
+	prompt: 'select_account',
+};
+
+Vue.use(GAuth, gAuthOption);
 
 Vue.config.productionTip = false;
 
