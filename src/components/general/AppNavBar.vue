0c541bb14233a6435ddbf3b167c69edba64564ef
Sanket Panda <pandasa123@gmail.com>
2020-03-10 13:41:16 -0400

    Separated the main App NavBar into its own component for readability

---
+++ src/components/general/AppNavBar.vue
@@ -0,0 +1,171 @@
+<template>
+	<nav v-if="['Landing'].includes($route.name) === false" class="bg-gray-800">
+		<div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<div class="relative flex items-center justify-between h-16">
+				<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+					<button
+						v-on:click="hamburgerOpen = !hamburgerOpen"
+						class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+					>
+						<hamburger :is-open="hamburgerOpen" />
+					</button>
+				</div>
+				<div
+					class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start"
+				>
+					<div class="flex-shrink-0">
+						<img
+							class="block w-auto h-8 lg:hidden"
+							src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg"
+							alt=""
+						/>
+						<img
+							class="hidden w-auto h-8 lg:block"
+							src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg"
+							alt=""
+						/>
+					</div>
+					<div class="hidden sm:block sm:ml-6">
+						<div class="flex">
+							<router-link
+								to="/dashboard"
+								:class="{
+									'text-white bg-gray-900': $route.name === 'Dashboard',
+								}"
+								class="px-3 py-2 text-sm font-medium leading-5 text-gray-300 transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+								>Dashboard</router-link
+							>
+							<router-link
+								to="/todo"
+								:class="{
+									'text-white bg-gray-900': $route.name === 'Todo',
+								}"
+								class="px-3 py-2 ml-4 text-sm font-medium leading-5 text-gray-300 transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+								>To-Do</router-link
+							>
+							<router-link
+								to="/calendar"
+								:class="{
+									'text-white bg-gray-900': $route.name === 'Calendar',
+								}"
+								class="px-3 py-2 ml-4 text-sm font-medium leading-5 text-gray-300 transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+								>Calendar</router-link
+							>
+						</div>
+					</div>
+				</div>
+				<div
+					class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+				>
+					<button
+						class="p-1 text-gray-400 transition duration-150 ease-in-out border-2 border-transparent rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+					>
+						<svg
+							class="w-6 h-6"
+							stroke="currentColor"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+							/>
+						</svg>
+					</button>
+					<div class="relative ml-3">
+						<button
+							v-on:click="settingsOpen = !settingsOpen"
+							class="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-white"
+						>
+							<img
+								class="w-8 h-8 rounded-full"
+								src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+								alt="Avatar"
+							/>
+						</button>
+						<transition
+							name="custom-classes-transition"
+							x-transition:enter="transition ease-out duration-100"
+							x-transition:enter-start="transform opacity-0 scale-95"
+							x-transition:enter-end="transform opacity-100 scale-100"
+							x-transition:leave="transition ease-in duration-75"
+							x-transition:leave-start="transform opacity-100 scale-100"
+							x-transition:leave-end="transform opacity-0 scale-95"
+						>
+							<div
+								v-if="settingsOpen"
+								class="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg"
+							>
+								<div class="py-1 font-medium bg-white rounded-md shadow-xs">
+									<router-link
+										to="/profile"
+										class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+										>Your Profile</router-link
+									>
+									<router-link
+										to="/settings"
+										class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+										>Settings</router-link
+									>
+									<!-- TODO: Add method to clean JWT -->
+									<router-link
+										to="/"
+										class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+										>Sign out</router-link
+									>
+								</div>
+							</div>
+						</transition>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div
+			:class="{ block: hamburgerOpen, hidden: !hamburgerOpen }"
+			class="sm:hidden"
+		>
+			<div class="px-2 pt-2 pb-3">
+				<router-link
+					to="/dashboard"
+					:class="{
+						'text-white bg-gray-900': $route.name === 'Dashboard',
+					}"
+					class="block px-3 py-2 text-base font-medium text-gray-300 transition duration-150 ease-in-out rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+					>Dashboard
+				</router-link>
+				<router-link
+					to="/todo"
+					:class="{
+						'text-white bg-gray-900': $route.name === 'Todo',
+					}"
+					class="block px-3 py-2 mt-1 text-base font-medium text-gray-300 transition duration-150 ease-in-out rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+					>To-Do</router-link
+				>
+				<router-link
+					to="/calendar"
+					:class="{
+						'text-white bg-gray-900': $route.name === 'Calendar',
+					}"
+					class="block px-3 py-2 mt-1 text-base font-medium text-gray-300 transition duration-150 ease-in-out rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+					>Calendar</router-link
+				>
+			</div>
+		</div>
+	</nav>
+</template>
+<script>
+import Hamburger from '@/components/buttons/Hamburger';
+export default {
+	components: {
+		Hamburger,
+	},
+	data() {
+		return {
+			hamburgerOpen: false,
+			settingsOpen: false,
+		};
+	},
+};
+</script>
