93d19858f9b9f451df0eab3b2f73fd84474ac2c8
Sanket Panda <pandasa123@gmail.com>
2020-03-21 14:53:33 -0400

    Temporarily left as monolithic component

---
+++ src/components/landing/LandingNavBar/LargeNav.vue
@@ -0,0 +1,69 @@
+<template>
+	<nav
+		class="relative flex items-center justify-between max-w-screen-xl px-4 mx-auto sm:px-6"
+	>
+		<div class="flex items-center flex-1">
+			<div class="flex items-center justify-between w-full md:w-auto">
+				<a href="/">
+					<img
+						class="w-auto h-8 sm:h-10"
+						src="@/assets/shortLogo.svg"
+						alt="Logo"
+					/>
+				</a>
+				<div class="flex items-center -mr-2 md:hidden">
+					<button
+						v-on:click="hamburgerOpen = !hamburgerOpen"
+						type="button"
+						class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+					>
+						<three-lines />
+					</button>
+				</div>
+			</div>
+			<div class="hidden md:block md:ml-10">
+				<a
+					href="#"
+					class="font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+				>
+					Plantain
+				</a>
+				<a
+					href="#"
+					class="ml-10 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+				>
+					Features
+				</a>
+				<a
+					href="#"
+					class="ml-10 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900"
+				>
+					Getting Started
+				</a>
+			</div>
+		</div>
+		<div class="hidden text-right md:block">
+			<span class="inline-flex rounded-md shadow-md">
+				<google-sign-in-button
+					classes="bg-purple-600 text-white rounded-md shadow-xs hover:text-gray-100 hover:bg-purple-700"
+				/>
+			</span>
+		</div>
+	</nav>
+</template>
+<script>
+import ThreeLines from '@/components/buttons/Hamburger/ThreeLines';
+import GoogleSignInButton from '@/components/buttons/GoogleSignIn/GoogleSignInButton.vue';
+
+export default {
+	components: {
+		ThreeLines,
+		GoogleSignInButton,
+	},
+	data: function() {
+		return {
+			hamburgerOpen: false,
+		};
+	},
+};
+</script>
---
+++ src/components/landing/LandingNavBar/SmallNav.vue
@@ -0,0 +1,80 @@
+<template>
+	<transition
+		name="custom-landing-nav-transition"
+		enter-active-class="duration-150 ease-out"
+		enter-class="scale-95 opacity-0"
+		enter-to-class="scale-100 opacity-100"
+		leave-active-class="duration-100 ease-in"
+		leave-class="scale-100 opacity-100"
+		leave-to-class="scale-95 opacity-0"
+	>
+		<div
+			class="absolute inset-x-0 top-0 p-2 md:hidden"
+			v-if="hamburgerOpen"
+			v-click-outside="closeHamburger"
+		>
+			<div class="transition origin-top-right transform rounded-lg shadow-md">
+				<div class="overflow-hidden bg-white rounded-lg shadow-xs">
+					<div class="flex items-center justify-between px-5 pt-4">
+						<div>
+							<img class="w-auto h-8" src="@/assets/shortLogo.svg" alt="" />
+						</div>
+						<div class="-mr-2">
+							<button
+								v-on:click="hamburgerOpen = false"
+								type="button"
+								class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+							>
+								<hamburger-close />
+							</button>
+						</div>
+					</div>
+					<div class="px-2 pt-2 pb-3">
+						<a
+							href="#"
+							class="block px-3 py-2 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+						>
+							Plantain
+						</a>
+						<a
+							href="#"
+							class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+						>
+							Features
+						</a>
+						<a
+							href="#"
+							class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
+						>
+							Getting Started
+						</a>
+					</div>
+					<landing-mini-button
+						classes="text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:bg-gray-100 focus:text-indigo-700"
+					/>
+				</div>
+			</div>
+		</div>
+	</transition>
+</template>
+<script>
+import LandingMiniButton from '@/components/buttons/GoogleSignIn/LandingMiniButton.vue';
+import HamburgerClose from '@/components/buttons/Hamburger/HamburgerClose';
+
+export default {
+	components: {
+		HamburgerClose,
+		LandingMiniButton,
+	},
+	data: function() {
+		return {
+			hamburgerOpen: false,
+		};
+	},
+	methods: {
+		closeHamburger: function() {
+			this.hamburgerOpen = false;
+		},
+	},
+};
+</script>
