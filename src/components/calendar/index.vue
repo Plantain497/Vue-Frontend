ff871f67835d12c382a23b2fd7eb99c49065a4d7
Sanket Panda <pandasa123@gmail.com>
2020-04-01 16:23:09 -0400

    Added fullcalendar with drag and drop, month view, and week view

---
+++ src/components/calendar/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<full-calendar
+		:header="header"
+		height="parent"
+		:views="views"
+		default-view="weekView"
+		contentHeight="auto"
+		:editable="true"
+		:selectable="true"
+		:columnHeaderFormat="{
+			weekday: 'short',
+			month: 'numeric',
+			day: 'numeric',
+			omitCommas: true,
+		}"
+		:plugins="calendarPlugins"
+		:events="events"
+	/>
+</template>
+<script>
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+export default {
+	components: {
+		FullCalendar,
+	},
+	data() {
+		return {
+			calendarPlugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+			header: {
+				left: 'title',
+				center: '',
+				right: 'today weekView,monthView prev,next',
+			},
+			views: {
+				weekView: {
+					type: 'timeGridWeek',
+					slotDuration: '01:00:00',
+					allDayText: 'All-Day',
+					nowIndicator: true,
+					buttonText: 'Week',
+				},
+				monthView: {
+					type: 'dayGridMonth',
+					buttonText: 'Month',
+				},
+			},
+			events: [
+				{ title: 'event 1', date: '2020-04-01' },
+				{ title: 'event 2', date: '2020-04-02' },
+			],
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+@import '~@fullcalendar/timegrid/main.css';
+</style>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,10 +1,14 @@
 <template>
-	<div class="px-2 py-8 mx-auto bg-gray-100 max-w-7xl sm:px-6 lg:px-8">
-		<h1>Calendar Page</h1>
+	<div class="h-full px-2 py-8 mx-auto bg-gray-100 max-w-7xl sm:px-6 lg:px-8">
+		<calendar />
 	</div>
 </template>
 <script>
+import Calendar from '@/components/calendar';
+
 export default {
-	name: 'Calendar',
+	components: {
+		Calendar,
+	},
 };
 </script>
