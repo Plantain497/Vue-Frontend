44b9d5cb98bf47733f9a920cb64f29452080f20c
Sanket Panda <pandasa123@gmail.com>
2020-04-05 15:26:03 -0400

    Added TextInput

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
 	root: true,
+
 	env: {
 		node: true,
 	},
+
 	extends: ['plugin:vue/essential', 'prettier'],
+
 	parserOptions: {
 		ecmaVersion: 2020,
 	},
+
 	rules: {
 		// 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -16,4 +20,16 @@ module.exports = {
 		'no-var-requires': 0,
 		'vue/html-indent': ['error', 'tab'], // enforce tabs in template
 	},
+
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)'
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
 };
---
+++ tests/unit/TextInput.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils';
+import TextInput from '@/components/inputs/TextInput.vue';
+
+describe('TextInput.vue', () => {
+	it('should render input with id', () => {
+		const testInputId = 'testId';
+		// Pass in props
+		const wrapper = mount(TextInput, {
+			propsData: {
+				inputId: testInputId,
+			},
+		});
+		// Find the TextInput's attributes so we can test id or aria-label
+		const attributes = wrapper.find('input').attributes();
+		// Validate equality
+		expect(attributes.id).toBe(testInputId);
+	});
+});
