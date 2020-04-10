eaa3b568c0fcb63ba4fdade33b34547eebb81381
Sanket Panda <pandasa123@gmail.com>
2020-04-10 14:51:25 -0400

    Updated styling if is complete + emit event if check

--- src/components/todo/TodoItem.vue
+++ src/components/todo/TodoItem.vue
@@ -3,23 +3,24 @@
 		class="flex justify-between px-6 py-4 pb-4 whitespace-no-wrap transition duration-200 ease-in-out border-b border-gray-200 rounded-sm rounded-md hover:shadow-md hover:bg-purple-50"
 	>
 		<div class="flex items-center truncate">
-			<input
-				:checked="isComplete"
-				v-on:click="isComplete = !isComplete"
-				type="checkbox"
-				class="form-checkbox"
-			/>
+			<input :checked="isComplete" v-on:click="completeTodo" type="checkbox" class="form-checkbox" />
 			<span class="truncate">
 				<div class="px-6 truncate">
-					<p class="text-sm leading-5 text-gray-900 truncate">{{ title }}</p>
-					<p class="text-sm leading-5 text-gray-500 truncate">{{ description }}</p>
+					<p
+						class="text-sm leading-5 truncate"
+						:class="{'task-complete': isComplete, 'text-gray-900': !isComplete}"
+					>{{ title }}</p>
+					<p
+						class="text-sm leading-5 truncate"
+						:class="{'task-complete': isComplete, 'text-gray-500': !isComplete}"
+					>{{ description }}</p>
 				</div>
 			</span>
 		</div>
 	</div>
 </template>
 <script>
-import Delete from '@/assets/Icons/Delete';
+// import Delete from '@/assets/Icons/Delete';
 
 export default {
 	name: 'TodoItem',
@@ -45,6 +46,15 @@ export default {
 		deletePrompt: function() {
 			this.$emit('deleteTodoId', { id: this.id, date: this.date });
 		},
+		completeTodo: function() {
+			this.$emit('completeTodo', { id: this.id });
+		},
 	},
 };
 </script>
+<style scoped>
+.task-complete {
+	@apply line-through;
+	@apply text-gray-400;
+}
+</style>
